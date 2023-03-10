c1b680108fe6299b228d2ead7a5a3ccb790b632d
flaviaadiniz <flaviaadiniz@gmail.com>
2023-03-10 10:04:15 -0300

    Initial commit

---
+++ Aula01.py
@@ -0,0 +1,23 @@
+idade = 38
+x = "Teste"
+
+print("Variável idade =", idade)
+print("Variável x =", x, "!")
+print()
+
+nome = input("Digite seu nome: ")
+print("Seu nome é:", nome)
+print()
+
+preco = float(input("Digite o preço do produto: "))
+print("O preço é ", preco)
+print("-------------------------------------------------")
+
+precoProduto1 = float(input("Digite o preço do produto 1: "))
+precoProduto2= float(input("Digite o preço do produto 2: "))
+
+precoTotal = precoProduto1 + precoProduto2
+print("Preço total:", precoTotal)
+
+
+
