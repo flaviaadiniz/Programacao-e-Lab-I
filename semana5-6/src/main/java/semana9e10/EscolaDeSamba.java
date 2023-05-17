95683679de4fcef01367d559130eb31e7c496aff
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-16 22:31:02 -0300

    Adição da classe Escola de Samba na semana 9e10

---
+++ semana5-6/src/main/java/semana9e10/EscolaDeSamba.java
@@ -0,0 +1,27 @@
+package semana9e10;
+
+public class EscolaDeSamba {
+
+    private String nome;
+    private char grupo;
+    private String dataDeCriacao;
+    private String sambaEnredo;
+    private int vezesCampea;
+    public String carnavalesco;
+    private int quantAlas;
+
+
+    public void desfilar() {
+    }
+
+    public int calcularNota(int nota) {
+        return nota;
+    }
+
+    public void criarIngresso(String vip) {
+    }
+
+    public void cadastrarPassista() {
+    }
+
+}
