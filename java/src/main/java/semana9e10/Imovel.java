0999afccb2d5b05a9ed84e8e8b50455f9ef9197b
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-20 16:16:16 -0300

    Adição das classes Livro e Imovel

---
+++ java/src/main/java/semana9e10/Imovel.java
@@ -0,0 +1,40 @@
+package semana9e10;
+
+public class Imovel {
+
+    private String tipo;
+    private String local;
+    private int ano;
+    public String imobiliaria;
+    private boolean valor;
+
+
+    public int calcularIPTU(int valor) {
+        return valor;
+    }
+
+    public void visualizarInfo() {
+
+    }
+
+    public boolean alugado() {
+        return false;
+    }
+
+    public boolean mobiliado() {
+        return false;
+    }
+
+    public String pontosReferencia() {
+        return "";
+    }
+
+    public String acesso(String rua) {
+        return rua;
+    }
+
+    public boolean escritura() {
+        return false;
+    }
+
+}
---
+++ java/src/main/java/semana9e10/Livro.java
@@ -0,0 +1,23 @@
+package semana9e10;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String editora;
+    private boolean disponivel;
+
+    public String cadastrarAutor(String nome) {
+        return nome;
+    }
+
+    public String verificarISBN() {
+        return "ISBN: ";
+    }
+
+    public boolean verificarDisponibilidade() {
+        return disponivel;
+    }
+
+}
