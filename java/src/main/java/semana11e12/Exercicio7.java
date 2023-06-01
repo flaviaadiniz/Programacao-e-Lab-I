2485e63e3c9ad5b5360e25ca962cedaccc7fb5bd
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-31 23:13:54 -0300

    Adição dos exercícios 6 e 7 das semanas 11 e 12

--- java/src/main/java/semana11e12/Exercicio7.java
+++ java/src/main/java/semana11e12/Exercicio7.java
@@ -2,7 +2,6 @@
 Exercício 7. Escreva um método que recebe um array de double e devolve a posição onde se encontra o maior valor do
 array. Se houver mais de um valor maior, devolver a posição da primeira ocorrência.
  */
-
 package semana11e12;
 
 public class Exercicio7 {
