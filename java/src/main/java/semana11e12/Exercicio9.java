f1e0993db553c5ee5672341e9d269699de68a0e1
flaviaadiniz <flaviaadiniz@gmail.com>
2023-06-01 10:14:06 -0300

    Adição de um método para transformar o array usando operador ternário na lógica

--- java/src/main/java/semana11e12/Exercicio9.java
+++ java/src/main/java/semana11e12/Exercicio9.java
@@ -18,12 +18,21 @@ public class Exercicio9 {
         }
     }
 
+    public static void transformarArrayOpTernario(int[] inteiros) {
+        for (int i = 0; i < inteiros.length; i++) {
+            inteiros[i] = inteiros[i] % 2 == 0 ? 1 : -1;
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] arrayTeste = {1, 2, 3, 4, 5, 6, 7};
-
         tranformaArray(arrayTeste);
         System.out.println(Arrays.toString(arrayTeste));
+
+        int[] arrayTeste2 = {1, 3, 5, 2, 4, 6};
+        transformarArrayOpTernario(arrayTeste2);
+        System.out.println(Arrays.toString(arrayTeste2));
 
 
     }
